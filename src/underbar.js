b093aec45b1f15f6e2c462807366d9d8e3a9b86f
j-ornelas <johnnyoh1988@gmail.com>
2017-12-16 17:13:20 -0800

    solve pt1 - reject

--- src/underbar.js
+++ src/underbar.js
@@ -95,6 +95,16 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var passes = [];
+    var fails = [];
+    for (var i=0;i<collection.length;i++){
+      if (test(collection[i]) === true){
+        passes.push(collection[i]);
+      } else {
+        fails.push(collection[i]);
+      }
+    }
+    return fails;
   };
 
   // Produce a duplicate-free version of the array.
